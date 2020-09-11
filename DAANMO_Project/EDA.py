1cd76e8defffea01ed341239a75440c9e26b0592
Dibmar <diomedes.barbero@gmail.com>
2020-09-11 18:15:48 +0200

    11/09/2020 DAANMO Project. Clean up on minor notebooks is finished, only cross-indexing remaing. Minor functions added to EDA

--- DAANMO_Project/EDA.py
+++ DAANMO_Project/EDA.py
@@ -5,6 +5,8 @@ from datetime import date
 import matplotlib.pyplot as plt
 import ast
 import pandas as pd
+from datetime import datetime
+import time
 
 def f_protected (x, i= 0):
     """
@@ -262,6 +264,36 @@ def crew_cleaner (df, role_name, function, condition_list):
     df[col_name_1] = df.loc[:, 'crew'].apply(condition_1_extractor)
     df[col_name_2] = df.loc[:, 'crew'].apply(condition_2_extractor)
 
+def personnel_separator (df, col_list):
+    """
+                        ---What it does---
+    Creates a new df_new object using a list of 3 columns from another df object. Use these function when the cells of the original df are in       list format.
+                        ---What it needs---
+    A df object (df)
+    A list of 34 columns from the df (col_list). These MUST correspond with those of the df.
+                        ---What it returns---
+    A new df object (df_new)
+    """
+
+    value_1_col = []
+    value_2_col = [] 
+    value_3_col = []
+
+    
+    for e in range(df.shape[0]):
+        for i in range(len(df.loc[e, col_list[1]])):
+            value_1 = df.loc[e, col_list[0]]
+            value_1_col.append(value_1)
+
+            value_2 = df.loc[e, col_list[1]][i]
+            value_2_col.append(value_2)
+            
+            value_3 = df.loc[e, col_list[2]][i]
+            value_3_col.append(value_3)
+            
+    df_new = pd.DataFrame({col_list[0] : value_1_col, col_list[1] : value_2_col, col_list[2] : value_3_col})
+    return df_new
+
 ### SPECIFIC TO MOVIE-METADATA NOTEBOOK
 def genre_counter (df, df2, col1):
     """
@@ -292,7 +324,7 @@ def single_condition_separator (df, col_list, col_search, cond_1):
     Separates the lists of countries in a production from the container and stores them in a new df in list format.
     ---What it needs---
         - A df object (df) with a json column to be extracted
-        - A list of columns to construct the new df with (col_list).ç
+        - A list of columns to construct the new df with (col_list).
         - The column to be searched (col_search)
         - A condition for separation (cond_1)
     ---What it returns---
@@ -303,9 +335,52 @@ def single_condition_separator (df, col_list, col_search, cond_1):
     df2 = df.copy()
     df3 = df2[col_list]
 
-    df3[col_list[1]] = df3[col_list[1]].fillna('[]').apply(literal_eval).apply(lambda x: [i[cond_1] for i in x] if isinstance(x, list) else [])
+    df3[col_search] = df3[col_search].fillna('[]').apply(ast.literal_eval).apply(lambda x: [i[cond_1] for i in x] if isinstance(x, list) else [])
     return df3
 
+### SPECIFIC TO KEYWORDS NOTEBOOK
+def keys_separator (df, col_list):
+    """
+                        ---What it does---
+    Creates a new df_new object using a list of 2 columns from another df object. Use these function when the cells of the original df are in       list format.
+                        ---What it needs---
+    A df object (df)
+    A list of 2 columns from the df (col_list). These MUST correspond with those of the df.
+                        ---What it returns---
+    A new df object (df_new)
+    """
+
+    value_1_col = []
+    value_2_col = [] 
+
+    
+    for e in range(df.shape[0]):
+        for i in range(len(df.loc[e, col_list[1]])):
+            value_1 = df.loc[e, col_list[0]]
+            value_1_col.append(value_1)
+
+            value_2 = df.loc[e, col_list[1]][i]
+            value_2_col.append(value_2)
+            
+    df_new = pd.DataFrame({col_list[0] : value_1_col, col_list[1] : value_2_col})
+    return df_new
+
+### SPECIFIC TO FIXER-NAMES
+def gender_for_name (df):
+    """
+                        ---What it does---
+    Takes the 'name' column and the 'gender' column of your df and applies the gender and capper lambdas. This will return you a capitalized column of strings (name) and a Female/Male column.
+                        ---What it needs---
+    A df with at leas two columns that MUST be named 'name' and 'gender'
+    """
+
+    gender = lambda x: "Female" if x == 'G' or x == 2 else ("Male" if x == 'B' or x == 1 else x)
+    capper = lambda e: e.capitalize()
+
+    df['gender'] = df['gender'].apply(gender)
+    df['name'] = df['name'].astype(str)
+    df['name'] = df['name'].apply(capper)
+    df['name'] = df['name'].drop_duplicates(keep = 'first')
 
 ### ----------------------------------
 
@@ -315,6 +390,8 @@ def single_condition_separator (df, col_list, col_search, cond_1):
 gender = lambda x: "Female" if x == 1 else ("Male" if x == 2 else x)
 
 rounder = lambda x: round(x)
+
+todatetime = lambda e: datetime.utcfromtimestamp(e).strftime('%Y-%m-%d') # %H:%M:%S ommited
 
                                     # GRÁFICOS
 
